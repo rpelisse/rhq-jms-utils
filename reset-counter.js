d961b46c1958a8311a1633d0730e36757581928c
Romain PELISSE <belaran@gmail.com>
2013-10-21 15:34:33 +0200

    pass queueId from script

--- reset-counter.js
+++ reset-counter.js
@@ -1,6 +1,11 @@
-var queueId=10197
+var DEBUG = false
 
+if (args.length != 1) {
+    throw "Missing required argument - Queue ID";
+}
+
+var queueId=args[0]
 
 var conf = new Configuration()
 OperationManager.scheduleResourceOperation(queueId, "resetMessageCounter", 10000, 100, 1, 10000, conf, "Reset Message Counter")
-pretty.print("Queue's message [" + queueId + "] counter has been reset")
+pretty.print("Queue's message [" + queueId + "] counter has been reset.")
